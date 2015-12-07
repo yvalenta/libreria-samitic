52e6228a005c2bdafd19b495a82537bcf11361b2
Yonatan Valencia <yvalenta@gmail.com>
2015-12-06 19:35:49 -0500

    - [X] Validaciones en modelo libro y reseña

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,6 @@ class Book < ActiveRecord::Base
   has_many :reviews
   mount_uploader :cover, CoverUploader
 
+  validates_presence_of :title, :description, :isbn
+  validates :isbn, uniqueness: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :book
   belongs_to :user
+
+  validates_presence_of :comment
+  validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "Entre 1 y 5"}
+  validates :user_id, uniqueness: true
 end
