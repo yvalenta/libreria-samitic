43e7ac9c090220b5f8cff808c2e9f745e5bf55da
Yonatan Valencia <yvalenta@gmail.com>
2015-12-06 21:03:27 -0500

    - [X] Cambio de tema en comentarios y ajustes varios de funcionalidad

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -26,10 +26,12 @@ class ReviewsController < ApplicationController
   # POST /reviews.json
   def create
     @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.book_id = @book.id
 
     respond_to do |format|
       if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
+        format.html { redirect_to @book, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
       else
         format.html { render :new }
