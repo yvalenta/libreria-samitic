06eb03ed7c5689b81f0be36a49b5aca3590bcd04
Yonatan Valencia <yvalenta@gmail.com>
2015-12-06 21:22:28 -0500

    - [X] Validacion a lo machete style para adicionar reseña y administrar libros

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  def self.is_admin?(user)
+    user.profile.eql? 1
+  end
 end
