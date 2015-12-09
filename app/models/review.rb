f255e527bfcc424b8c0034393688c09b783fd49a
Yonatan Valencia <yvalenta@gmail.com>
2015-12-08 19:39:44 -0500

    - [X] Se agregan condicionales en la vista de libros :C para editar y eliminar comentario segun perfil y segun creador

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,9 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of :comment
-  validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "Entre 1 y 5"}
+  validates :score, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "Between 1 y 5"}
   validates :user_id, uniqueness: {scope: :book_id}
+  def self.my_review?(user, review)
+    review.user_id.eql? user
+  end
 end
